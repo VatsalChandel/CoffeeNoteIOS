2a6b110790ab09875641688d4434e57004951c75
ChandelVatsal <68132483+ChandelVatsal@users.noreply.github.com>
2025-12-28 03:06:17 -0800

    Initial Commit

---
+++ CoffeeNote/CoffeeNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeNoteApp.swift
+//  CoffeeNote
+//
+//  Created by Vatsal Chandel on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
